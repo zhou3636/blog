592659a9de2dd22a001135c5dfbaa5402dfba111
zhou3636 <zhou3534@gmail.com>
2025-05-27 23:23:24 +0800

    1

--- docs/css/list.js
+++ docs/css/list.js
@@ -12,29 +12,28 @@ async function init() {
         renderTags();
         renderArticles();
     } catch (error) {
-        console.error('加载数据失败:',error.message);
+        console.error('加载数据失败:', error.message);
     }
 }
 // 渲染文章列表
 function renderArticles() {
     const container = document.getElementById('left-column');
     const filtered = articles.filter(article => {
-        const matchSearch = article.title.includes(currentSearch) || 
-                          article.description.includes(currentSearch);
+        const matchSearch = article.title.includes(currentSearch) ||
+            article.description.includes(currentSearch);
         const matchTag = !currentTag || article.tags.includes(currentTag);
         return matchSearch && matchTag;
     });
 
     container.innerHTML = filtered.map(article => `
-        <a href="view.html?file=${encodeURIComponent(article.file)}"  target="_blank" class="article-card">
-            <div class="article-title">${article.title}</div>
-            <div class="article-description">${article.description}</div>
-            <div class="article-tags">
-                ${article.tags.map(tag => `
-                    <span class="article-tag">${tag}</span>
-                `).join('')}
-            </div>
-        </a>
+    <a href="view.html?file=${encodeURIComponent(article.file)}"  target="_blank" class="article-card">
+        <div class="incard">   
+          <div class="article-title">${article.title}</div>
+          <div class="article-description">${article.description}</div>
+          <div class="article-tags">${article.tags.map(tag => `<span class="article-tag">${tag}</span>`).join('')}</div>
+        </div>
+        <img class="cardimg" src="image/kk.jpg" alt="">
+    </a>
     `).join('');
 }
 
@@ -42,7 +41,7 @@ function renderArticles() {
 function renderTags() {
     const tags = [...new Set(articles.flatMap(article => article.tags))];
     const container = document.getElementById('tagsContainer');
-    
+
     container.innerHTML = [
         '<div class="tag" data-tag="">全部</div>',
         ...tags.map(tag => `
