52fcd3e95f4140a05631f676d5b50af5e09abd9e
zhou3636 <zhou3534@gmail.com>
2025-06-05 11:39:14 +0800

    Update avatar.js

--- docs/css/avatar.js
+++ docs/css/avatar.js
@@ -1,17 +1,24 @@
-
 function showhdimg() {
     const dialog = document.getElementById('hdimg');
+    
+    // 检查是否已经有图片了
+    const existingImg = dialog.querySelector('img[src]');
+    if (existingImg) {
+        dialog.showModal();
+        return;
+    }
     
     // 显示加载动画
     dialog.innerHTML = '<div class="loading"></div>';
     dialog.showModal();
     
-    // 创建新的图片元素
-    const img = new Image();
+    // 创建图片元素
+    const img = document.createElement('img');
     
     // 图片加载完成后显示
     img.onload = function() {
-        dialog.innerHTML = `<img src="image/nxd-2.jpeg" onclick="hdimg.close()">`;
+        dialog.innerHTML = '';
+        dialog.appendChild(img);
     };
     
     // 开始加载图片
