fbbed7acb5c2f21ae845033d1835446f94cc33c5
zhou3636 <zhou3534@gmail.com>
2025-06-13 13:20:42 +0800

    1

--- docs/js/imglist.js
+++ docs/js/imglist.js
@@ -1,35 +1,32 @@
-
-// v2
 async function img() {
     try {
         // 读取JSON文件
         const response = await fetch('img/img.json');
         const data = await response.json();
-        
+
         // 获取容器元素
         const container = document.getElementById('pts');
-        
+
         // 清空容器
         container.innerHTML = '';
-        
+
         // 遍历数组生成HTML
         data.forEach(item => {
             const div = document.createElement('div');
+            div.className = 'photo-card';
             div.innerHTML = `
-                <div class="photo-card">
-                    <div class="photo" data-src="img/${item.file}" onclick="showImage(this)">
-                    <img class="img-s" src="img2/${item.file}" alt="图片" onerror="this.style.display='none'">
-                    </div>
-                    <span class='title-p1'>${item.title}</span>
-                    <span class='title-p2'>${item.title1}</span>
-                </div>  
+                <div class="photo" data-src="img/${item.file}" onclick="showImage(this)">
+                <img class="img-s" src="img2/${item.file}" alt="图片" onerror="this.style.display='none'">
+                </div>
+                <span class='title-p1'>${item.title}</span>
+                <span class='title-p2'>${item.title1}</span>
             `;
             container.appendChild(div);
         });
-        
+
     } catch (error) {
         console.error('加载图片列表失败:', error);
-        document.getElementById('pts').innerHTML = '<p style="color: red;">加载图片列表失败，请检查img.json文件是否存在。</p>';
+        document.getElementById('pts').innerHTML = '<p style="color: red;">加载图片列表失败</p>';
     }
 }
 
